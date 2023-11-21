bfc4fee7554da97b30048bd8bc2fd0c5b384cb69
Abhishek3689 <abhisek.nishad3689@gmail.com>
2023-11-21 17:39:49 +0530

    first commit

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.linear_model import ElasticNet
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 import numpy as np
-
+from urllib.parse import urlparse
 import logging
 
 logging.basicConfig(level=logging.WARN)
@@ -57,7 +57,21 @@ with mlflow.start_run():
     mlflow.log_metric("r2", r2)
     mlflow.log_metric("mae", mae)
 
-    mlflow.sklearn.log_model(lr, "model")
+    remote_server_uri = "https://dagshub.com/Abhishek3689/MLflow_UI.mlflow"
+    mlflow.set_tracking_uri(remote_server_uri)
+
+    tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+    # Model registry does not work with file store
+    if tracking_url_type_store != "file":
+        # Register the model
+        # There are other ways to use the Model Registry, which depends on the use case,
+        # please refer to the doc for more information:
+        # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+        mlflow.sklearn.log_model(
+            lr, "model", registered_model_name="ElasticnetWineModel")
+    else:
+        mlflow.sklearn.log_model(lr, "model")
 
 
 
